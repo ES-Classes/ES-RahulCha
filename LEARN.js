959f046991e04ee9f420ff670ba84cb8b0a721e2
Rahul-cha <anengaan@gmail.com>
2022-04-03 11:35:38 +0545

    ahh

--- LEARN.js
+++ LEARN.js
@@ -1,1 +1,17 @@
 // install
+console.log('pramodsir');
+// datatypes - 8
+//Numbers - FP
+
+var population = 323451;
+console.log(1/0);
+console.log('sss'/14);
+
+var bignumber = 22222222222222222222222222222222111111111n;
+ console.log(bignumber);
+
+ //String - Sequence of characters 
+ var name = ' Bishnu Rawal';
+  name = "Bishnu Rawal";
+  name= `Bishnu Rawal`;
+console.log(name);
