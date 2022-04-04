5d37fe6ff9f45fe11becb7d7c51f88f033607ba2
Rahul-cha <anengaan@gmail.com>
2022-04-04 17:01:28 +0545

    init2

--- day2.js
+++ day2.js
@@ -1,1 +1,55 @@
-// install
+// lesson 2 JS
+console.log("Hello there!");
+
+var age = 45;
+age = 'bishnu';
+
+// Datatypes - 8
+// Numbers - FP
+var population = 32423234;
+var bigNuber = 32423535345435354353803984209482048092840920948204248n;
+console.log(1/0);
+console.log("sdfsf"/34);
+console.log(bigNuber);
+
+//String - Sequcnce of characters
+var name = 'Bishnu Rawal';
+name = "Bishnu Rawal";
+name = `Bishnu Rawal`;
+
+console.log(`My name is ${1 + 5 * 6}`);
+
+// Boolean - true or false
+var isAnimal = true;
+
+// Undefined
+var year;
+console.log(year);
+
+// Null
+var u = null;
+
+//Object
+//Symbol
+
+console.log(typeof name);
+typeof 0
+typeof 45n
+typeof true
+typeof 'sdfs'
+typeof Symbol('id')
+typeof Math
+typeof null
+
+//let constant var
+var a = 454;
+let b = 34;
+const c = 67;
+
+
+//operators
+let x = 90;
+let y = 90;
+let x1 = x++;
+let y1 = ++y;
+console.log(x, y, x1, y1);
