bef1bfc791fd54066010bac134a0c8e7de6e0bef
Rahul-cha <anengaan@gmail.com>
2022-04-17 10:22:56 +0545

    a

--- lession5-StringAndTemplateLiteral.js
+++ lession5-StringAndTemplateLiteral.js
@@ -6,3 +6,19 @@ const d = new String('Vedas');
 
 
 console.log(a.length);
+console.log(a.charAt(1));
+console.log(a);
+
+let cc ='क';
+let ccc = 'क';
+let dd= 'ख';
+
+console.log(cc.localeCompare(ccc));
+
+
+//TemplateLiteral
+let para = `sdfsfsfs+ 
+sdsdsdsd`;
+
+//stromg interpolation
+console.log(` My college is ${a + b + c} `); //stringma dynamic embbed garne
